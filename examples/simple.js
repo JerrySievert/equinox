70624a2e1517bd7db4e5fb2e5d58a0276745babe
Jerry Sievert <code@legitimatesounding.com>
2016-04-02 21:43:42 -0700

    update to include basic test runner working

--- examples/simple.js
+++ examples/simple.js
@@ -2,8 +2,8 @@ var tests = [
   {
     'should equal 1': function ( ) {
       var result = plv8.execute('SELECT 1 AS num');
-      assert.equal(result.length, 1);
-      assert.equal(result[0].num, 1);
+      assert.equal(result.length, 1, "length should be 1");
+      assert.equal(result[0].num, 1, "num should equal 0");
     }
   }
 ];
