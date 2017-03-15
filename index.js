744bf4833d0ce1e2bc9430e983ce6711d4fbec8c
Jerry Sievert <code@legitimatesounding.com>
2017-03-14 20:09:48 -0700

    add runner to index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var db = require('./lib/db');
+var db     = require('./lib/db');
 var loader = require('./lib/loader');
-var util = require('./lib/util');
+var util   = require('./lib/util');
+var runner = require('./lib/runners/async');
 
-exports.db = db;
+exports.db     = db;
 exports.loader = loader;
-exports.util = util;
+exports.util   = util;
+exports.runner = runner;
